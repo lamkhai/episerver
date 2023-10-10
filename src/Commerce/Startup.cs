cba5cea7bf41d91db9f1b6d7afffdbc711436426
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-10 10:08:39 +0700

    Install Commerce project

---
+++ src/Commerce/Startup.cs
@@ -0,0 +1,55 @@
+using EPiServer.Cms.Shell;
+using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Scheduler;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+using Mediachase.Commerce.Anonymous;
+
+namespace Commerce
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _webHostingEnvironment;
+
+        public Startup(IWebHostEnvironment webHostingEnvironment)
+        {
+            _webHostingEnvironment = webHostingEnvironment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_webHostingEnvironment.IsDevelopment())
+            {
+                AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(_webHostingEnvironment.ContentRootPath, "App_Data"));
+
+                services.Configure<SchedulerOptions>(options => options.Enabled = false);
+            }
+
+            services
+                .AddCmsAspNetIdentity<ApplicationUser>()
+                .AddCommerce()
+                .AddAdminUserRegistration()
+                .AddEmbeddedLocalization<Startup>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAnonymousId();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapContent();
+            });
+        }
+    }
+}
