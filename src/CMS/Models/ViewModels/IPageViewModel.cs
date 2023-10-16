1c9dee530425e2ab97f814d37758058341b7caad
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-16 12:42:22 +0700

    Research Renderings - Create a block preview template

---
+++ src/CMS/Models/ViewModels/IPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CMS.Models.Pages;
+
+namespace CMS.Models.ViewModels;
+
+public interface IPageViewModel<out T> where T : SitePageData
+{
+    T CurrentPage { get; }
+
+    LayoutModel Layout { get; set; }
+
+    IContent Section { get; set; }
+}
