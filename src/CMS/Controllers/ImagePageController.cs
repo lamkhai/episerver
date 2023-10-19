851c836fa79ba08b3459068be857edce358dd8ff
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-19 13:54:46 +0700

    Research Media support - Adding an image link property

---
+++ src/CMS/Controllers/ImagePageController.cs
@@ -0,0 +1,17 @@
+﻿using CMS.Models.Pages;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers;
+
+public class ImagePageController : PageController<ImagePage>
+{
+    public ActionResult Index(ImagePage currentPage)
+    {
+        /* Implementation of action. 
+         * You can create your own view model class that you pass to the view
+         * or you can pass the page type for simpler templates */
+
+        return View(currentPage);
+    }
+}
---
+++ src/CMS/Models/Pages/ImagePage.cs
@@ -0,0 +1,20 @@
+﻿using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models.Pages;
+
+[ContentType(
+        DisplayName = "ImagePage",
+        GUID = "61f028c8-be3d-4942-b58c-65ea8b28e7b6",
+        Description = "Description for this image page type")]
+public class ImagePage : PageData
+{
+    [CultureSpecific]
+    [Display(
+      Name = "Page image",
+      Description = "Link to image that will be displayed on the page.",
+      GroupName = SystemTabNames.Content,
+      Order = 1)]
+    [UIHint(UIHint.Image)]
+    public virtual ContentReference Image { get; set; }
+}
