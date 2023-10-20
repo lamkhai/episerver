b2304a238a8737072124252d75583ab8cb846741
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-20 10:14:58 +0700

    Research Security - Integrate Azure AD using OpenID Connect

--- src/CMS/Startup.cs
+++ src/CMS/Startup.cs
@@ -2,10 +2,15 @@ using EPiServer.Cms.Shell;
 using EPiServer.Cms.UI.AspNetIdentity;
 using EPiServer.Framework.Localization.XmlResources;
 using EPiServer.Scheduler;
+using EPiServer.Security;
 using EPiServer.ServiceLocation;
 using EPiServer.Web;
 using EPiServer.Web.Routing;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using System.Collections.Specialized;
+using System.Security.Claims;
+using System.Text;
 
 namespace CMS
 {
@@ -49,6 +54,8 @@ namespace CMS
                 //    o.AddProvider<MyCustomBlobProvider>("anotherCustomBlobProvider");
                 //    o.DefaultProvider = "anotherCustomBlobProvider";
                 //});
+
+                ConfigureAzureADService(services);
             }
 
             services
@@ -76,13 +83,79 @@ namespace CMS
                 endpoints.MapContent();
             });
         }
+
+        private void ConfigureAzureADService(IServiceCollection services)
+        {
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "azure-cookie";
+                    options.DefaultChallengeScheme = "azure";
+                })
+                .AddCookie("azure-cookie", options =>
+                {
+                    options.Events.OnSignedIn = async ctx =>
+                    {
+                        if (ctx.Principal?.Identity is ClaimsIdentity claimsIdentity)
+                        {
+                            // Syncs user and roles so they are available to the CMS
+                            var synchronizingUserService = ctx.HttpContext.RequestServices.GetRequiredService<ISynchronizingUserService>();
+                            await synchronizingUserService.SynchronizeAsync(claimsIdentity);
+                        }
+                    };
+                })
+                .AddOpenIdConnect("azure", options =>
+                {
+                    options.SignInScheme = "azure-cookie";
+                    options.SignOutScheme = "azure-cookie";
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.CallbackPath = "/signin-oidc";
+                    options.UsePkce = true;
+
+                    // If Azure AD is register for multi-tenant
+                    //options.Authority = "https://login.microsoftonline.com/" + "common" + "/v2.0";
+                    options.Authority = "https://login.microsoftonline.com/" + "tenant id" + "/v2.0";
+                    options.ClientId = "client id";
+
+                    options.Scope.Clear();
+                    options.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+                    options.Scope.Add(OpenIdConnectScope.OfflineAccess);
+                    options.Scope.Add(OpenIdConnectScope.Email);
+                    options.MapInboundClaims = false;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        RoleClaimType = ClaimTypes.Role,
+                        NameClaimType = "preferred_username",
+                        ValidateIssuer = false
+                    };
+
+                    options.Events.OnRedirectToIdentityProvider = ctx =>
+                    {
+                        // Prevent redirect loop
+                        if (ctx.Response.StatusCode == 401)
+                        {
+                            ctx.HandleResponse();
+                        }
+
+                        return Task.CompletedTask;
+                    };
+
+                    options.Events.OnAuthenticationFailed = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.BodyWriter.WriteAsync(Encoding.ASCII.GetBytes(context.Exception.Message));
+                        return Task.CompletedTask;
+                    };
+                });
+        }
     }
 }
 
 public static class ContentAreaTags
 {
     public const string FullWidth = "Full";
-	 public const string TwoThirdsWidth = "Wide";
-	 public const string HalfWidth = "Half";
-	 public const string OneThirdWidth = "Narrow";
+    public const string TwoThirdsWidth = "Wide";
+    public const string HalfWidth = "Half";
+    public const string OneThirdWidth = "Narrow";
 }
