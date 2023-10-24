0392e5f72f3715927f44cf1b1d9851496498a6db
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-24 17:28:25 +0700

    Research Catalog content - Bundles and packages - Get the entries of a bundle/package

---
+++ src/Foundation/Features/CatalogContent/Services/BundleService.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Commerce.Catalog.Linking;
+
+namespace Foundation.Features.CatalogContent.Services;
+
+// Research: Get the entries of a bundle
+
+public interface IBundleService
+{
+    IEnumerable<BundleEntry> ListBundleEntries(ContentReference referenceToBundle); // Retrieve entries from a bundle
+}
+
+public class BundleService : IBundleService
+{
+    public IEnumerable<BundleEntry> ListBundleEntries(ContentReference referenceToBundle)
+    {
+        var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
+
+        // Relations to bundle entries are of type BundleEntry
+        var bundleEntries = relationRepository.GetChildren<BundleEntry>(referenceToBundle);
+        return bundleEntries;
+    }
+}
---
+++ src/Foundation/Features/CatalogContent/Services/PackageService.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Commerce.Catalog.Linking;
+
+namespace Foundation.Features.CatalogContent.Services;
+
+// Research: Get the entries of a package
+
+public interface IPackageService
+{
+    IEnumerable<PackageEntry> ListPackageEntries(ContentReference referenceToPackage); // Retrieve entries from a package
+}
+
+public class PackageService : IPackageService
+{
+    public IEnumerable<PackageEntry> ListPackageEntries(ContentReference referenceToPackage)
+    {
+        var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
+
+        // Relations to package entries are of type PackageEntry
+        var packageEntries = relationRepository.GetChildren<PackageEntry>(referenceToPackage);
+        return packageEntries;
+    }
+}
