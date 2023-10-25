05de905d652aa73168a3de81b6e679f1745fa3f2
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 15:31:10 +0700

    Research Catalog content - Catalog content provider - Get children of a category

--- src/Foundation/Features/NewProducts/NewProductsPageController.cs
+++ src/Foundation/Features/NewProducts/NewProductsPageController.cs
@@ -29,6 +29,26 @@ namespace Foundation.Features.NewProducts
             return View(model);
         }
 
+        [HttpGet("GetChildrenOfACategory"), AllowAnonymous] // https://localhost:44397/en/new-arrivals/GetChildrenOfACategory?nodeIdFromCommerce=
+        public IActionResult GetChildrenOfACategory(int nodeIdFromCommerce)
+        {
+            //Get the currently configured content loader and reference converter from the service locator
+            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            var referenceConverter = ServiceLocator.Current.GetInstance<ReferenceConverter>();
+
+            //Get the correct category id as it's represented in EPiServer Commerce
+            //In this example we arbitrarily use the integer 1
+            //var nodeIdFromCommerce = 1;
+
+            //We use the content link builder to get the contentlink to our product
+            var productLink = referenceConverter.GetContentLink(nodeIdFromCommerce, CatalogContentType.CatalogNode, 0);
+
+            //Get the children using CMS API
+            var children = contentLoader.GetChildren<CatalogContentBase>(productLink);
+
+            return Ok(children.Select(x => ConvertToResponse(x)));
+        }
+
         [HttpGet("LoadAProduct"), AllowAnonymous] // https://localhost:44397/en/new-arrivals/LoadAProduct?productId=
         public IActionResult LoadAProduct(int productId)
         {
@@ -45,18 +65,23 @@ namespace Foundation.Features.NewProducts
             //The commerce content name represents the name of the product
             //var productName = productContent.Name;
 
-            return Ok(new
+            return Ok(ConvertToResponse(productContent));
+        }
+
+        private object ConvertToResponse(CatalogContentBase content)
+        {
+            return new
             {
-                productContent.ContentGuid,
-                productContent.CatalogId,
-                productContent.ContentType,
-                productContent.ContentTypeID,
-                productContent.Name,
-                productContent.RouteSegment,
-                productContent.Status,
-                productContent.StartPublish,
-                productContent.StopPublish,
-            });
+                content.ContentGuid,
+                content.CatalogId,
+                content.ContentType,
+                content.ContentTypeID,
+                content.Name,
+                content.RouteSegment,
+                content.Status,
+                content.StartPublish,
+                content.StopPublish,
+            };
         }
     }
 }
