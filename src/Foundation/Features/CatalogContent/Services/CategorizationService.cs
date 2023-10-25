3ba4e6c1471937d3ff9240a2b10d6ec4f7618632
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 10:10:37 +0700

    Research Catalog content - Categorizations - Adding a categorization to an entry or category

--- src/Foundation/Features/CatalogContent/Services/CategorizationService.cs
+++ src/Foundation/Features/CatalogContent/Services/CategorizationService.cs
@@ -4,11 +4,24 @@ namespace Foundation.Features.CatalogContent.Services;
 
 public interface ICategorizationService
 {
+    void AddCategory(ContentReference referenceToEntryOrCategory, ContentReference referenceToCategory);
     IEnumerable<NodeRelation> ListCategories(ContentReference referenceToEntryOrCategory);
 }
 
 public class CategorizationService : ICategorizationService
 {
+    public void AddCategory(ContentReference referenceToEntryOrCategory, ContentReference referenceToCategory)
+    {
+        var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
+        var newCategory = new NodeRelation
+        {
+            SortOrder = 100,
+            Child = referenceToEntryOrCategory,
+            Parent = referenceToCategory
+        };
+        relationRepository.UpdateRelation(newCategory);
+    }
+
     public IEnumerable<NodeRelation> ListCategories(ContentReference referenceToEntryOrCategory)
     {
         var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
