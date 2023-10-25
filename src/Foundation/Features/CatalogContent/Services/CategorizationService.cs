2b35cea7f1680a53a4265c3aa07ff454617c2ad8
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 10:10:11 +0700

    Research Catalog content - Categorizations - Get the categorizations of an entry or category

---
+++ src/Foundation/Features/CatalogContent/Services/CategorizationService.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Commerce.Catalog.Linking;
+
+namespace Foundation.Features.CatalogContent.Services;
+
+public interface ICategorizationService
+{
+    IEnumerable<NodeRelation> ListCategories(ContentReference referenceToEntryOrCategory);
+}
+
+public class CategorizationService : ICategorizationService
+{
+    public IEnumerable<NodeRelation> ListCategories(ContentReference referenceToEntryOrCategory)
+    {
+        var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
+        var categories = relationRepository.GetChildren<NodeRelation>(referenceToEntryOrCategory);
+        return categories;
+    }
+}
