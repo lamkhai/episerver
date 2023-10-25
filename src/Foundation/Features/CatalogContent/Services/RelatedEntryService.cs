f25c0d5e81a3ad90b119824e02fd7462b39876c0
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 12:03:54 +0700

    Research Catalog content - Related entries - Add a related entry to an entry

--- src/Foundation/Features/CatalogContent/Services/RelatedEntryService.cs
+++ src/Foundation/Features/CatalogContent/Services/RelatedEntryService.cs
@@ -4,11 +4,35 @@ namespace Foundation.Features.CatalogContent.Services;
 
 public interface IRelatedEntryService
 {
+    void AddAssociation(ContentReference referenceToEntry, ContentReference referenceToRelatedEntry);
     IEnumerable<Association> ListAssociations(ContentReference referenceToEntry);
 }
 
 public class RelatedEntryService : IRelatedEntryService
 {
+    public void AddAssociation(ContentReference referenceToEntry, ContentReference referenceToRelatedEntry)
+    {
+        var associationRepository = ServiceLocator.Current.GetInstance<IAssociationRepository>();
+        var newAssociation = new Association
+        {
+            Group = new AssociationGroup
+            {
+                Name = "CrossSell",
+                Description = "",
+                SortOrder = 100
+            },
+            SortOrder = 100,
+            Source = referenceToEntry,
+            Target = referenceToRelatedEntry,
+            Type = new AssociationType
+            {
+                Id = AssociationType.DefaultTypeId,
+                Description = ""
+            }
+        };
+        associationRepository.UpdateAssociation(newAssociation);
+    }
+
     public IEnumerable<Association> ListAssociations(ContentReference referenceToEntry)
     {
         var associationRepository = ServiceLocator.Current.GetInstance<IAssociationRepository>();
