9b8a1559184d1d27055e531647c5bbc66deff544
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 10:22:59 +0700

    Research Catalog content - Related entries - Get related entries for an entry

---
+++ src/Foundation/Features/CatalogContent/Services/RelatedEntryService.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Commerce.Catalog.Linking;
+
+namespace Foundation.Features.CatalogContent.Services;
+
+public interface IRelatedEntryService
+{
+    IEnumerable<Association> ListAssociations(ContentReference referenceToEntry);
+}
+
+public class RelatedEntryService : IRelatedEntryService
+{
+    public IEnumerable<Association> ListAssociations(ContentReference referenceToEntry)
+    {
+        var associationRepository = ServiceLocator.Current.GetInstance<IAssociationRepository>();
+        var associations = associationRepository.GetAssociations(referenceToEntry);
+        return associations;
+    }
+}
