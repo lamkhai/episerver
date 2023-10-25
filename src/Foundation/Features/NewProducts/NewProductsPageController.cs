cdeb089a9693dce5d08c810c356cae106b84eb30
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 15:25:05 +0700

    Research Catalog content - Catalog content provider - Load a product

--- src/Foundation/Features/NewProducts/NewProductsPageController.cs
+++ src/Foundation/Features/NewProducts/NewProductsPageController.cs
@@ -8,8 +8,9 @@ namespace Foundation.Features.NewProducts
         private readonly ISearchService _searchService;
         private readonly ISettingsService _settingsService;
 
-        public NewProductsPageController(ISearchService searchService,
-           ISettingsService settingsService)
+        public NewProductsPageController(
+            ISearchService searchService,
+            ISettingsService settingsService)
         {
             _searchService = searchService;
             _settingsService = settingsService;
@@ -26,6 +27,36 @@ namespace Foundation.Features.NewProducts
             };
 
             return View(model);
+        }
+
+        [HttpGet("LoadAProduct"), AllowAnonymous] // https://localhost:44397/en/new-arrivals/LoadAProduct?productId=
+        public IActionResult LoadAProduct(int productId)
+        {
+            //Get the currently configured content loader and reference converter from the service locator
+            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            var referenceConverter = ServiceLocator.Current.GetInstance<ReferenceConverter>();
+
+            //We use the content link builder to get the contentlink to our product
+            var productLink = referenceConverter.GetContentLink(productId, CatalogContentType.CatalogEntry, 0);
+
+            //Get the product using CMS API
+            var productContent = contentLoader.Get<CatalogContentBase>(productLink);
+
+            //The commerce content name represents the name of the product
+            //var productName = productContent.Name;
+
+            return Ok(new
+            {
+                productContent.ContentGuid,
+                productContent.CatalogId,
+                productContent.ContentType,
+                productContent.ContentTypeID,
+                productContent.Name,
+                productContent.RouteSegment,
+                productContent.Status,
+                productContent.StartPublish,
+                productContent.StopPublish,
+            });
         }
     }
 }
