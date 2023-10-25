b4a539dba47fc32a4cf4d3b7bb93fc835481947b
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-25 10:11:02 +0700

    Research Catalog content - Categorizations - Removing a categorization from an entry or category

--- src/Foundation/Features/CatalogContent/Services/CategorizationService.cs
+++ src/Foundation/Features/CatalogContent/Services/CategorizationService.cs
@@ -6,6 +6,7 @@ public interface ICategorizationService
 {
     void AddCategory(ContentReference referenceToEntryOrCategory, ContentReference referenceToCategory);
     IEnumerable<NodeRelation> ListCategories(ContentReference referenceToEntryOrCategory);
+    void RemoveCategory(ContentReference referenceToEntryOrCategory, ContentReference referenceToCategory);
 }
 
 public class CategorizationService : ICategorizationService
@@ -27,5 +28,20 @@ public class CategorizationService : ICategorizationService
         var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
         var categories = relationRepository.GetChildren<NodeRelation>(referenceToEntryOrCategory);
         return categories;
+    }
+
+    public void RemoveCategory(ContentReference referenceToEntryOrCategory, ContentReference referenceToCategory)
+    {
+        var relationRepository = ServiceLocator.Current.GetInstance<IRelationRepository>();
+        // Define a relation matching the one to remove, or use
+        // GetRelations to find the one you want to remove and pass that to
+        // RemoveRelation
+        var relationToRemove = new NodeRelation
+        {
+            Child = referenceToEntryOrCategory,
+            Parent = referenceToCategory
+        };
+        // Removes matching NodeRelation, or no action if no match exists
+        relationRepository.RemoveRelation(relationToRemove);
     }
 }
