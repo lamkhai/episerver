54cd029be4e6dd88bafa9148b8a8103b41fadd03
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-26 18:10:14 +0700

    Research Orders - Order manipulation - Load orders

--- src/Foundation/Features/LKBuilders/CartBuilder.cs
+++ src/Foundation/Features/LKBuilders/CartBuilder.cs
@@ -1,9 +1,22 @@
 ﻿namespace Foundation.Features.LKBuilders;
 
-public interface ICartBuilder : IOrderBuilder<ICart>
+public interface ICartBuilder<TCart> : IOrderBuilder<TCart>
+    where TCart : class, ICart
 {
+    TCart LoadCart(string orderTypeName);
+    TCart LoadOrCreateCart(string orderTypeName);
 }
 
-public class CartBuilder : OrderBuilder<ICart>, ICartBuilder
+public class CartBuilder<TCart> : OrderBuilder<TCart>, ICartBuilder<TCart>
+    where TCart : class, ICart
 {
+    public virtual TCart LoadCart(string orderTypeName)
+    {
+        return OrderRepository.LoadCart<TCart>(ContactId, orderTypeName);
+    }
+
+    public virtual TCart LoadOrCreateCart(string orderTypeName)
+    {
+        return OrderRepository.LoadOrCreateCart<TCart>(ContactId, orderTypeName);
+    }
 }
--- src/Foundation/Features/LKBuilders/OrderBuilder.cs
+++ src/Foundation/Features/LKBuilders/OrderBuilder.cs
@@ -7,16 +7,34 @@ public interface IOrderBuilder<TOrderGroup>
     where TOrderGroup : class, IOrderGroup
 {
     TOrderGroup Create(string name);
+    TOrderGroup Load(int orderGroupId);
+    TOrderGroup Load(OrderReference orderReference);
+    IEnumerable<TOrderGroup> LoadAllForContactId(string name);
 }
 
 public class OrderBuilder<TOrderGroup> : IOrderBuilder<TOrderGroup>
     where TOrderGroup : class, IOrderGroup
 {
     protected readonly IOrderRepository OrderRepository = ServiceLocator.Current.GetInstance<IOrderRepository>();
+    protected readonly Guid ContactId = PrincipalInfo.CurrentPrincipal.GetContactId();
 
     public virtual TOrderGroup Create(string name)
     {
-        var contactId = PrincipalInfo.CurrentPrincipal.GetContactId();
-        return OrderRepository.Create<TOrderGroup>(contactId, name);
+        return OrderRepository.Create<TOrderGroup>(ContactId, name);
+    }
+
+    public virtual TOrderGroup Load(int orderGroupId)
+    {
+        return OrderRepository.Load<TOrderGroup>(orderGroupId);
+    }
+
+    public virtual TOrderGroup Load(OrderReference orderReference)
+    {
+        return OrderRepository.Load(orderReference) as TOrderGroup;
+    }
+
+    public virtual IEnumerable<TOrderGroup> LoadAllForContactId(string name)
+    {
+        return OrderRepository.Load<TOrderGroup>(ContactId, name);
     }
 }
