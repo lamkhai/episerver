5ab7644973ac168e8261224f804e687e8f6c5234
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-27 14:10:43 +0700

    Research Orders - Order manipulation - Work with shipments

--- src/Foundation/Features/LKBuilders/OrderGroupFactoryBuilder.cs
+++ src/Foundation/Features/LKBuilders/OrderGroupFactoryBuilder.cs
@@ -1,15 +1,18 @@
 ﻿namespace Foundation.Features.LKBuilders;
 
-public interface IOrderGroupFactoryBuilder
+public interface IOrderGroupFactoryBuilder<TOrderGroup>
+    where TOrderGroup : class, IOrderGroup
 {
-    IOrderForm CreateOrderForm<TOrderGroup>(TOrderGroup orderGroup, OrderFormModel model) where TOrderGroup : class, IOrderGroup;
+    IOrderForm CreateOrderForm(TOrderGroup orderGroup, OrderFormModel model);
+    void WorkWithShipment(TOrderGroup orderGroup);
 }
 
-public class OrderGroupFactoryBuilder : IOrderGroupFactoryBuilder
+public class OrderGroupFactoryBuilder<TOrderGroup> : IOrderGroupFactoryBuilder<TOrderGroup>
+    where TOrderGroup : class, IOrderGroup
 {
     protected readonly IOrderGroupFactory OrderGroupFactory = ServiceLocator.Current.GetInstance<IOrderGroupFactory>();
 
-    public virtual IOrderForm CreateOrderForm<TOrderGroup>(TOrderGroup orderGroup, OrderFormModel model) where TOrderGroup : class, IOrderGroup
+    public virtual IOrderForm CreateOrderForm(TOrderGroup orderGroup, OrderFormModel model)
     {
         var orderForm = OrderGroupFactory.CreateOrderForm(orderGroup);
         orderGroup.Forms.Add(orderForm);
@@ -23,6 +26,28 @@ public class OrderGroupFactoryBuilder : IOrderGroupFactoryBuilder
         }
 
         return orderForm;
+    }
+
+    public virtual void WorkWithShipment(TOrderGroup orderGroup)
+    {
+        //Create and add shipment to first form
+        var shipment = OrderGroupFactory.CreateShipment(orderGroup);
+
+        //Pass in OrderGroupFactory for unit testing as a from will be created if there is none o
+        orderGroup.AddShipment(shipment, OrderGroupFactory);
+
+        //Set address after adding to collection because of limitation in implementation
+        shipment.ShippingAddress = OrderGroupFactory.CreateOrderAddress(orderGroup);
+
+        //Create and add shipment to second form (b2b)
+        var secondForm = orderGroup.Forms.Last();
+        orderGroup.AddShipment(secondForm, shipment);
+
+        //Remove shipment from first form
+        orderGroup.GetFirstForm().Shipments.Remove(shipment);
+
+        //Remove shipment from second form (b2b)
+        orderGroup.Forms.Last().Shipments.Remove(shipment);
     }
 }
 
