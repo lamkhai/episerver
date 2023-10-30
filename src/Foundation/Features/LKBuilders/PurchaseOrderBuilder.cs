f6708df4132aae6d9f86136bd1c21db8706abaac
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-10-30 21:02:03 +0700

    Research Orders - Order processing - Process purchase orders (Hold, Release, Cancel, Process)

--- src/Foundation/Features/LKBuilders/PurchaseOrderBuilder.cs
+++ src/Foundation/Features/LKBuilders/PurchaseOrderBuilder.cs
@@ -2,11 +2,37 @@
 
 public interface IPurchaseOrderBuilder : IOrderBuilder<IPurchaseOrder>
 {
+    OrderProcessingResult CancelOrder(IPurchaseOrder purchaseOrder);
+    OrderProcessingResult HoldOrder(IPurchaseOrder purchaseOrder);
+    OrderProcessingResult ProcessOrder(IPurchaseOrder purchaseOrder);
+    OrderProcessingResult ReleaseOrder(IPurchaseOrder purchaseOrder);
 }
 
 public class PurchaseOrderBuilder : OrderBuilder<IPurchaseOrder>, IPurchaseOrderBuilder
 {
+    protected readonly IPurchaseOrderProcessor PurchaseOrderProcessor = ServiceLocator.Current.GetInstance<IPurchaseOrderProcessor>();
+
     public PurchaseOrderBuilder(IOrderGroupFactoryBuilder<IPurchaseOrder> orderGroupFactoryBuilder) : base(orderGroupFactoryBuilder)
     {
+    }
+
+    public virtual OrderProcessingResult CancelOrder(IPurchaseOrder purchaseOrder)
+    {
+        return PurchaseOrderProcessor.CancelOrder(purchaseOrder);
+    }
+
+    public virtual OrderProcessingResult HoldOrder(IPurchaseOrder purchaseOrder)
+    {
+        return PurchaseOrderProcessor.HoldOrder(purchaseOrder);
+    }
+
+    public virtual OrderProcessingResult ProcessOrder(IPurchaseOrder purchaseOrder)
+    {
+        return PurchaseOrderProcessor.ProcessOrder(purchaseOrder);
+    }
+
+    public virtual OrderProcessingResult ReleaseOrder(IPurchaseOrder purchaseOrder)
+    {
+        return PurchaseOrderProcessor.ReleaseOrder(purchaseOrder);
     }
 }
