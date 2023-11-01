9b395932dd359ec64703294d0bec7e5827949f92
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-11-01 17:19:29 +0700

    Research Marketing and campaigns

---
+++ src/Foundation/Features/MarketingAndCampaigns/CampaignPartialController.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Framework.DataAnnotations;
+
+namespace Foundation.Features.MarketingAndCampaigns;
+
+[TemplateDescriptor(Default = true)]
+public class CampaignPartialController : PartialContentComponent<SalesCampaign>
+{
+    protected override IViewComponentResult InvokeComponent(SalesCampaign currentCampaign)
+    {
+        // Implementation of action view the page
+        return View(currentCampaign);
+    }
+}
