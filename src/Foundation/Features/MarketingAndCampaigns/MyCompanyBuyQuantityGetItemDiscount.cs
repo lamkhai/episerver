783ac300958db301c2e300b5216c594787122f97
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-11-01 18:05:57 +0700

    Research Marketing and campaigns - Promotions

---
+++ src/Foundation/Features/MarketingAndCampaigns/MyCompanyBuyQuantityGetItemDiscount.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.Commerce.Marketing.Promotions;
+
+namespace Foundation.Features.MarketingAndCampaigns;
+
+[ContentType(GUID = "be97060d-622c-4811-94fa-d3c1c5f18eb8")]
+[AvailableContentTypes(Include = new[] { typeof(PromotionData) })]
+public class MyCompanyBuyQuantityGetItemDiscount : BuyQuantityGetItemDiscount
+{
+    [Display(Order = 13, GroupName = SystemTabNames.PageHeader, Prompt = "Other Info")]
+    public virtual string OtherInfo { get; set; }
+}
---
+++ src/Foundation/Features/MarketingAndCampaigns/MyCompanyBuyQuantityGetItemDiscountPartialController.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Framework.DataAnnotations;
+
+namespace Foundation.Features.MarketingAndCampaigns;
+
+[TemplateDescriptor(Default = true)]
+public class MyCompanyBuyQuantityGetItemDiscountPartialController : PartialContentController<MyCompanyBuyQuantityGetItemDiscount>
+{
+    public ActionResult Index(MyCompanyBuyQuantityGetItemDiscount currentDiscount)
+    {
+        // Implementation of action view the page. 
+        return View(currentDiscount);
+    }
+}
