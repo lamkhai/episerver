d0024c035c5e6c642ab3841b38f8bcde099f05c0
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2023-11-02 10:00:39 +0700

    Research Marketing and campaigns - Custom promotions

---
+++ src/Foundation/Features/MarketingAndCampaigns/CustomPromotionBlock.cs
@@ -0,0 +1,8 @@
+﻿namespace Foundation.Features.MarketingAndCampaigns;
+
+[ContentType(GUID = "15B7BEA8-967A-4C5C-87F3-7346E71CBCC9")]
+public class CustomPromotionBlock : BlockData
+{
+    public virtual int RequiredQuantity { get; set; }
+    public virtual IList<ContentReference> Targets { get; set; }
+}
